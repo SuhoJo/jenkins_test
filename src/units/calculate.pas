c7cec26203965957ddd9b063186e4f41f176c2b8
suhojo <suhojo@woowahan.com>
2019-03-20 16:40:42 +0900

    unit test passed

--- src/units/calculate.pas
+++ src/units/calculate.pas
@@ -14,7 +14,7 @@ implementation
 
 function TCalculate.Add(A, B: uint32): uint32;
 begin
-    Result := A + succ(B); // succ -> 서수 표현식/ 리턴값 : 표현식 값의 다음 값
+    Result := A + B;
 end;
 
-end.
+end.                                                                            s
